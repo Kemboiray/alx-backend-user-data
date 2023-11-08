3c36b899d1ad3f0f5f399954b5dd93ea9a32d294
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-08 23:48:55 +0300

    Task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module defines the class `BasicAuth`"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """This class inherits from `Auth`"""
+    pass
