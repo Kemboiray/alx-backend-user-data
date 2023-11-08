da09a77bc2dca8e0adacd05b7cf3407a25eab0ae
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-09 00:00:04 +0300

    Task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 """This module defines the class `BasicAuth`"""
 from api.v1.auth.auth import Auth
+from typing import Union
 
 
 class BasicAuth(Auth):
     """This class inherits from `Auth`"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> Union[str, None]:
+        """Return the Base64 part of the Authorization header"""
+        if not isinstance(authorization_header, str):
+            return None
+        return authorization_header.split(
+            " ")[1] if authorization_header.startswith("Basic ") else None
