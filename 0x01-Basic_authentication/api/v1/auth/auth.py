e59d12ca3c0c7619bed8f4d00140a3eeb40b4fb3
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-08 22:47:29 +0300

    Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,10 @@ class Auth:
     """This class provides methods to manage the API authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """TODO: Implement later"""
-        return False
+        """Return True if authentication is required for `path`"""
+        if path is None or not excluded_paths:
+            return True
+        return not path.rstrip("/") in [i.rstrip("/") for i in excluded_paths]
 
     def authorization_header(self, request=None) -> Union[str, None]:
         """TODO: Implement later"""
