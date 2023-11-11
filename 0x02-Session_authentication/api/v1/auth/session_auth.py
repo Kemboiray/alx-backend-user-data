def4b1ccf6be43dc0ec74faccae14c393a8e4f11
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-11 18:39:07 +0000

    Task 2[

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""This module defines the `SessionAuth` class"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """This class inherits from `Auth`"""
+
+    pass
