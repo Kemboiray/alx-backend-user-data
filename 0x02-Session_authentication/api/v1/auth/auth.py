4a696d09f0146cc675045fb622304e4c24086fc9
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-12 16:27:51 +0300

    Task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 """This module defines the class `Auth`"""
 
-from flask import request
-from typing import List, TypeVar, Union
+# from flask import request
+import typing as t
+import os
+
+SESSION_NAME = os.getenv("SESSION_NAME")
 
 
 class Auth:
     """This class provides methods to manage the API authentication"""
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: t.List[str]) -> bool:
         """Return True if authentication is required for `path`"""
         if path is None or not excluded_paths:
             return True
         return not path.rstrip("/") in [i.rstrip("/") for i in excluded_paths]
 
-    def authorization_header(self, request=None) -> Union[str, None]:
+    def authorization_header(self,
+                             request: t.Any = None) -> t.Union[str, None]:
         """
         Return the value of the `Authorization` header or `None` if missing
         """
@@ -22,6 +26,10 @@ class Auth:
             return None
         return request.headers.get("Authorization")
 
-    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+    def current_user(self, request=None) -> t.TypeVar('User'):  # type: ignore
         """TODO: Implement later"""
         pass
+
+    def session_cookie(self, request=None) -> t.Union[str, None]:
+        if request is not None:
+            return request.cookies.get(SESSION_NAME)
