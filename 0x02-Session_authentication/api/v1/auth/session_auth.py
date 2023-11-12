96ff113b91bdcf87726a6895053598b1831a7b6c
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-12 07:56:18 +0000

    Task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,19 @@
 # -*- coding: utf-8 -*-
 """This module defines the `SessionAuth` class"""
 
+import typing as t
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """This class inherits from `Auth`"""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: t.Union[str,
+                                              None]) -> t.Union[str, None]:
+        if isinstance(user_id, str):
+            self.session_id = str(uuid4())
+            SessionAuth.user_id_by_session_id[self.session_id] = user_id
+            return self.session_id
