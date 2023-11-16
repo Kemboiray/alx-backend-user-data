eff45461183e1cf4c8492954554ee2716797c8ef
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-16 18:15:21 +0300

    Task 0

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,8 @@
+# alx-backend-user-data
+
+## 0x03-user_authentication_service
+This project is a series of tasks re:
+  * How to declare API routes in a Flask app
+  * How to get and set cookies
+  * How to retrieve request form data
+  * How to return various HTTP status codes
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+This module defines a SQLAlchemy model, `User` for a database table, `users`.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    A class used to represent a user in the database.
+
+    Attributes
+    ----------
+    id : int
+        a unique value for each user
+    email : str
+        the user's email address
+    hashed_password : str
+        the user's password (hashed)
+    session_id : str
+        a unique session ID for each user
+    reset_token : str
+    """
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(length=250), nullable=False)
+    hashed_password = Column(String(length=250), nullable=False)
+    session_id = Column(String(length=250), nullable=True)
+    reset_token = Column(String(length=250), nullable=True)
+
+    def __repr__(self):
+        """Return the canonical string representation of `User`."""
+        return f"<User(name='{self.name}', email='{self.email}')>"
+
+    def __str__(self):
+        """Return a string representation of `User`."""
+        return f"{self.name} <{self.email}>"
