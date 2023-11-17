2b7a59f970ed606221e20bee23a7d087bfe4d4d4
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-17 23:34:24 +0000

    Task 1

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,3 +63,11 @@ class Auth:
                 return self._db.find_user_by(session_id=session_id)
             except NoResultFound:
                 return
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy the user session."""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            setattr(user, "session_id", None)
+        except NoResultFound:
+            return
