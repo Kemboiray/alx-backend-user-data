9b45cdde108bf0b57c575b1c2a32dcac71c943c6
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-17 23:14:09 +0300

    Task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,9 +21,18 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Register a user and return the `User` object"""
+        """Register a user and return the `User` object."""
         try:
             self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password).decode())
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate user credentials."""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False
