165088d0749e652ac19a08ab7f5a3877b64b6b19
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-17 12:33:12 +0300

    Remove exception handler

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,11 +34,7 @@ class DB:
                  hashed_password: str) -> User:
         """Add a user to the DB
         """
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
