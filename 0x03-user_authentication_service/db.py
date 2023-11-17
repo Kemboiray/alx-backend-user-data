139ff4b86deee5bf8773220da9cecf48f3fa5a79
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-17 13:10:30 +0300

    Task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,7 @@
 from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 from user import Base, User
 import typing as t
@@ -38,3 +39,15 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Return the first row found in the users table
+        as filtered by the input arguments
+        """
+        try:
+            result = self._session.query(User).filter_by(**kwargs).first()
+        except Exception as e:
+            raise(e)
+        if not result:
+            raise NoResultFound
+        return result
